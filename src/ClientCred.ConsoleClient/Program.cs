5839a9e4441742c11d3d6ccfd07e78a6666949a2
Russell T. Griffith <russell.griffith@evenflo.com>
2020-05-13 13:41:33 -0400

    Added Console App Example

---
+++ src/ClientCred.ConsoleClient/Program.cs
@@ -0,0 +1,54 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ClientCred.ConsoleClient
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var client = new HttpClient();
+
+            var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+            if (disco.IsError) 
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(
+                new ClientCredentialsTokenRequest{
+                    Address = disco.TokenEndpoint,
+                    ClientId = "ApiClient",
+                    ClientSecret = "apisecret",
+                    Scope = "auth.web.api"
+                });
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            // call api
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await apiClient.GetAsync("https://localhost:44390/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+        }
+    }
+}
